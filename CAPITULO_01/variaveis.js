e1ce7854e0b12f42da47009f6e4e44df475d57b0
michel <michel.ferreira.melo@gmail.com>
2021-11-20 18:14:21 -0300

    criando e mostrando as variáveis

---
+++ CAPITULO_01/variaveis.js
@@ -0,0 +1,25 @@
+//A linguagem JavaScript não é fortemente tipada. Assim, vejamos como as variáveis são decladas
+
+var num = 1; //declaração da variável (é uma BOA PRÁTICA declarar a variável)
+num2 = 2; //declaração da variável 
+
+num = 3; // atualizando a variável num. (é uma BOA PRÁTICA sempre que for atualizar a variável usar o mesmo tipo da original)
+
+var price = 1.5; //declarando um número decimal de ponto flutuante
+
+var myName = 'Michel'; //declarando uma string
+
+var trueValue = true; //declarando um booleano
+
+var nullVar = null; //declarando um valor null
+
+var und; //declarando uma variável undefined
+
+//imprimindo no console 
+console.log('num: ' + num);
+console.log('num2: ',num2);
+console.log('myName: ', myName);
+console.log('price: ',price)
+console.log('trueValue: ',trueValue);
+console.log('nullVar: ',nullVar);
+console.log('undefined: ',und);
