1b0683424819352c19f6332534003791867faedb
michel <michel.ferreira.melo@gmail.com>
2021-11-22 20:48:16 -0300

    LAÇOS

---
+++ CAPITULO_01/scripts/lacos.js
@@ -0,0 +1,17 @@
+// O laço for é igual ao de C e de Java.
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+var j = 0;
+while(j < 10){
+    console.log(j);
+    j++;
+}
+
+var k = 0;
+do{
+    console.log(k);
+    k++
+}while(k < 10);
+
