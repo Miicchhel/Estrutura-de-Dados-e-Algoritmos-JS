230a04b8697641cc8f1952a903da65146470d0e7
michel <michel.ferreira.melo@gmail.com>
2022-04-06 11:01:59 -0300

    Templates literais

---
+++ CAPITULO_02/scripts/templates-literais.js
@@ -0,0 +1,11 @@
+const book = {
+    name: 'Learning JavaScript dataStructures and Algorithms'
+};
+
+//concatenação de strings
+console.log('You are reading ' + book.name + '.\n And this is a new line\n and so is this.');
+
+//templates literais devem estar entre crases(` `)
+console.log(`You are reading ${book.name}. 
+    And this is a new line
+        and so is this.`);
